e624af0f41cb7875bcf7da74e09442d5405ffdce
aort <artashesano@icloud.com>
2022-10-13 17:31:31 +0400

    use custom widget instead of ListTile in todo_tile

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -30,49 +30,57 @@ class _TodoTileState extends State<TodoTile> {
   Widget build(BuildContext context) {
     return Consumer<Todos>(builder: (context, data, _) {
       final todo = data.getTodoById(widget.id);
-      return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10.0),
-        child: ListTile(
-          onLongPress: () => toggleTodoState(true),
-          leading: SizedBox(
-            width: MediaQuery.of(context).size.width - 150,
-            child: TextFormField(
-              focusNode: focusNode,
-              controller: taskController,
-              enabled: enabled,
-              cursorColor: Colors.blueGrey,
-              decoration: const InputDecoration(border: InputBorder.none),
-              style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                  color: Colors.blueGrey,
-                  decoration: todo.checked
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none),
-            ),
-          ),
-          trailing: Row(
-            mainAxisSize: MainAxisSize.min,
+      return GestureDetector(
+        onLongPress: () => toggleTodoState(true),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              if (enabled) ...[
-                IconButton(
-                  onPressed: () {
-                    toggleTodoState(false);
-                    taskController.text = todo.task;
-                  },
-                  icon: const Icon(Icons.close),
+              Expanded(
+                flex: enabled ? 7 : 9,
+                child: TextFormField(
+                  focusNode: focusNode,
+                  controller: taskController,
+                  enabled: enabled,
+                  cursorColor: Colors.blueGrey,
+                  decoration: const InputDecoration(border: InputBorder.none),
+                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                      color: Colors.blueGrey,
+                      decoration: todo.checked
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none),
                 ),
-                IconButton(
-                  onPressed: () {
-                    toggleTodoState(false);
-                    data.editTodo(todo.id, taskController.text);
-                    TodosIO.editTodo(todo);
-                  },
-                  icon: const Icon(Icons.check),
-                )
-              ] else
-                Checkbox(
-                  checked: todo.checked,
-                  onTap: () => data.toggleCheck(widget.id),
+              ),
+              Expanded(
+                flex: enabled ? 3 : 1,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    if (enabled) ...[
+                      IconButton(
+                        onPressed: () {
+                          toggleTodoState(false);
+                          taskController.text = todo.task;
+                        },
+                        icon: const Icon(Icons.close),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          toggleTodoState(false);
+                          data.editTodo(todo.id, taskController.text);
+                          TodosIO.editTodo(todo);
+                        },
+                        icon: const Icon(Icons.check),
+                      )
+                    ] else
+                      Checkbox(
+                        checked: todo.checked,
+                        onTap: () => data.toggleCheck(widget.id),
+                      ),
+                  ],
                 ),
+              ),
             ],
           ),
         ),
