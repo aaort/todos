a56b7d0edd596523ad320895c2a8fb874aad62b2
aort <artashesano@icloud.com>
2022-10-14 14:24:22 +0400

    add const constructor in home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -63,7 +63,7 @@ class Home extends StatelessWidget {
                 ],
               ),
             ),
-            TodoList(),
+            const TodoList(),
           ],
         ),
       ),
