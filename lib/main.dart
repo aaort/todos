2801ec0181700a6d52c441f7895bf46a277191f8
aort <artashesano@icloud.com>
2022-10-14 14:35:17 +0400

    put ChangeNotifierProvider down deeper in the tree

--- lib/main.dart
+++ lib/main.dart
@@ -12,39 +12,39 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<Todos>(
-      create: (_) => Todos(),
-      child: MaterialApp(
-        title: 'Todos',
-        theme: ThemeData(
-          scaffoldBackgroundColor: Colors.blueGrey,
-          textTheme: const TextTheme(
-            titleMedium: TextStyle(
-              color: Colors.white,
-              fontSize: 40.0,
-              fontWeight: FontWeight.w800,
-            ),
-            bodyMedium: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-            ),
-            bodySmall: TextStyle(
-              color: Colors.black,
-              fontSize: 20.0,
-            ),
-            bodyLarge: TextStyle(
-              color: Colors.blueGrey,
-              fontSize: 30.0,
-              fontWeight: FontWeight.w600,
-            ),
+    return MaterialApp(
+      title: 'Todos',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.blueGrey,
+        textTheme: const TextTheme(
+          titleMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 40.0,
+            fontWeight: FontWeight.w800,
           ),
-          iconTheme: const IconThemeData(
+          bodyMedium: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+          ),
+          bodySmall: TextStyle(
+            color: Colors.black,
+            fontSize: 20.0,
+          ),
+          bodyLarge: TextStyle(
             color: Colors.blueGrey,
-            size: 27,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w600,
           ),
         ),
-        home: const Home(),
+        iconTheme: const IconThemeData(
+          color: Colors.blueGrey,
+          size: 27,
+        ),
+      ),
+      home: ChangeNotifierProvider<Todos>(
+        create: (_) => Todos(),
+        child: const Home(),
       ),
     );
   }
