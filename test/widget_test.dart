5731c6780c3e0505e23b46a5d2136106b5bf408f
aort <artashesano@icloud.com>
2022-10-23 15:17:20 +0400

    add some tests for add_todo

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -27,5 +27,26 @@ void main() {
     await tester.tap(addTodoButton);
     await tester.pump();
     expect(addTodoModal, findsOneWidget);
+
+    final createTodoButton = find.byKey(const Key('createTodoButtonId'));
+    expect(tester.widget<ElevatedButton>(createTodoButton).enabled, false);
+
+    final createTodoInput = find.byKey(const Key('createTodoInputId'));
+    expect(createTodoButton, findsOneWidget);
+
+    const inputContent = 'something really important';
+    await tester.enterText(createTodoInput, inputContent);
+    await tester.pump();
+    final createTodoInputText = find.text(inputContent);
+    expect(createTodoInputText, findsOneWidget);
+
+    expect(tester.widget<ElevatedButton>(createTodoButton).enabled, true);
+
+    await tester.tap(createTodoButton);
+    await tester.pumpAndSettle(const Duration(seconds: 2));
+    // Wait until hide animation's complete
+    expect(
+        addTodoModal.evaluate().first.renderObject!.debugNeedsPaint, isFalse);
+    // Checks whether widget is visible or not (true if visible)
   });
 }
