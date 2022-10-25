363d302687995f70503a1c2743212e08aaa62c93
aort <artashesano@icloud.com>
2022-10-25 20:00:12 +0400

    reduce the elevation of add new todo floating button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -22,6 +22,7 @@ class Home extends StatelessWidget {
       child: Scaffold(
         resizeToAvoidBottomInset: false,
         floatingActionButton: FloatingActionButton(
+          elevation: 2.0,
           backgroundColor: Colors.blueGrey,
           onPressed: () => showAddTodoModal(context),
           child: const Icon(Icons.edit_outlined),
