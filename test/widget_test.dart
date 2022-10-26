2aa32ef86263baf39d67dddb6e446f1996caccb9
aort <artashesano@icloud.com>
2022-10-26 12:13:22 +0400

    move add_todo into screens folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:todos/main.dart';
 import 'package:todos/screens/home.dart';
-import 'package:todos/widgets/add_todo.dart';
+import 'package:todos/screens/add_todo.dart';
 import 'package:todos/widgets/todo_list.dart';
 import 'package:todos/widgets/todo_tile.dart';
 
