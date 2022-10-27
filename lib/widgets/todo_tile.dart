7603e35b253fe65c30696d5e56793580245446f3
aort <artashesano@icloud.com>
2022-10-27 12:48:41 +0400

    reduce nesting in todo_tile by removing Consumer

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -37,77 +37,76 @@ class _TodoTileState extends State<TodoTile> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<Todos>(builder: (context, data, _) {
-      final todo = data.getTodoById(widget.id);
-      return Dismissible(
-        onDismiss: () => onDeleteTodo(todo),
-        child: GestureDetector(
-          onLongPress: toggleTodoState,
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  child: Focus(
-                    onFocusChange: onFocusChange,
-                    child: TextFormField(
-                      autofocus: true,
-                      focusNode: focusNode,
-                      controller: taskController,
-                      enabled: enabled,
-                      cursorColor: Colors.blueGrey,
-                      textCapitalization: TextCapitalization.sentences,
-                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                          decoration: todo.checked
-                              ? TextDecoration.lineThrough
-                              : TextDecoration.none),
-                    ),
+    final todo = context.watch<Todos>().getTodoById(widget.id);
+    return Dismissible(
+      onDismiss: () => onDeleteTodo(todo),
+      child: GestureDetector(
+        onLongPress: toggleTodoState,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                child: Focus(
+                  onFocusChange: onFocusChange,
+                  child: TextFormField(
+                    autofocus: true,
+                    focusNode: focusNode,
+                    controller: taskController,
+                    enabled: enabled,
+                    cursorColor: Colors.blueGrey,
+                    textCapitalization: TextCapitalization.sentences,
+                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                        decoration: todo.checked
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      if (enabled) ...[
-                        IconButton(
-                          padding: const EdgeInsets.only(right: 10.0),
-                          constraints: const BoxConstraints(),
-                          onPressed: () => onDiscard(todo.task),
-                          icon: const Icon(Icons.close),
-                        ),
-                        IconButton(
-                          padding: const EdgeInsets.only(right: 10.0),
-                          constraints: const BoxConstraints(),
-                          // TODO: implement reminder editing
-                          onPressed: () {},
-                          icon: const Icon(Icons.timer_outlined),
-                        ),
-                        IconButton(
-                          padding: EdgeInsets.zero,
-                          constraints: const BoxConstraints(),
-                          onPressed: () {
-                            final newTodo = todo;
-                            newTodo.task = taskController.text;
-                            onEditTodo(newTodo);
-                          },
-                          icon: const Icon(Icons.check),
-                        )
-                      ] else
-                        Checkbox(
-                          checked: todo.checked,
-                          onTap: () => data.toggleCheck(widget.id),
-                        ),
-                    ],
-                  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    if (enabled) ...[
+                      IconButton(
+                        padding: const EdgeInsets.only(right: 10.0),
+                        constraints: const BoxConstraints(),
+                        onPressed: () => onDiscard(todo.task),
+                        icon: const Icon(Icons.close),
+                      ),
+                      IconButton(
+                        padding: const EdgeInsets.only(right: 10.0),
+                        constraints: const BoxConstraints(),
+                        // TODO: implement reminder editing
+                        onPressed: () {},
+                        icon: const Icon(Icons.timer_outlined),
+                      ),
+                      IconButton(
+                        padding: EdgeInsets.zero,
+                        constraints: const BoxConstraints(),
+                        onPressed: () {
+                          final newTodo = todo;
+                          newTodo.task = taskController.text;
+                          onEditTodo(newTodo);
+                        },
+                        icon: const Icon(Icons.check),
+                      )
+                    ] else
+                      Checkbox(
+                        checked: todo.checked,
+                        onTap: () =>
+                            context.read<Todos>().toggleCheck(widget.id),
+                      ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      );
-    });
+      ),
+    );
   }
 
   void onDiscard(String initialTask) {
