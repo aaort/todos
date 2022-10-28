09eb2c72ce4cf85910bdce21326877da8d93462c
aort <artashesano@icloud.com>
2022-10-28 14:19:11 +0400

    transfer todo_tile to stateless widget

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -3,34 +3,32 @@ import 'package:provider/provider.dart';
 import 'package:todos/logic/todo_actions.dart';
 import 'package:todos/logic/todos.dart';
 import 'package:todos/screens/todo_editor.dart';
+import 'package:todos/styles.dart';
 import 'package:todos/widgets/checkbox.dart';
 import 'package:todos/widgets/dismissible.dart';
 import 'package:todos/widgets/modal_bottom_sheet.dart';
 
-class TodoTile extends StatefulWidget {
+class TodoTile extends StatelessWidget {
   final String id;
 
   const TodoTile({super.key, required this.id});
 
-  @override
-  State<TodoTile> createState() => _TodoTileState();
-}
-
-class _TodoTileState extends State<TodoTile> {
-  bool enabled = false;
-
-  void onLongPress() async {
-    final todo = context.read<Todos>().getTodoById(widget.id);
+  void onLongPress(BuildContext context) async {
+    final todo = context.read<Todos>().getTodoById(id);
     popupModalBottomSheet(context: context, child: AddTodo(initialTodo: todo));
+  }
+
+  void toggleCheck(BuildContext context) {
+    context.read<Todos>().toggleCheck(id);
   }
 
   @override
   Widget build(BuildContext context) {
-    final todo = context.watch<Todos>().getTodoById(widget.id);
+    final todo = context.watch<Todos>().getTodoById(id);
 
     return DismissibleTile(
       onDismiss: () => TodoActions(context, todo).onDeleteTodo(todo),
-      onLongPress: onLongPress,
+      onLongPress: () => onLongPress(context),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
         child: Row(
@@ -41,14 +39,14 @@ class _TodoTileState extends State<TodoTile> {
                 todo.task * 4,
                 maxLines: 2,
                 overflow: TextOverflow.ellipsis,
-                style: Theme.of(context).textTheme.bodySmall,
+                style: Styles(context).getTodoTextStyle(todo.checked),
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(left: 20.0),
               child: Checkbox(
                 checked: todo.checked,
-                onTap: () => context.read<Todos>().toggleCheck(widget.id),
+                onTap: () => toggleCheck(context),
               ),
             ),
           ],
