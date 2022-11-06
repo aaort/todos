2149c1cb0041aaf08ab1c4b55f1e586a2f318336
aort <artashesano@icloud.com>
2022-11-06 17:40:52 +0400

    use date_time_format package for showing reminder text

--- lib/helpers/date.dart
+++ lib/helpers/date.dart
@@ -1,15 +1,7 @@
+import 'package:date_time_format/date_time_format.dart';
+
 String getReminderText(DateTime dateTime) {
-  final diff = dateTime.difference(DateTime.now());
-  if (diff.isNegative) {
-    return 'Reminded';
-  } else if (diff.inMinutes < 60) {
-    return 'Remind me in ${(diff.inSeconds / 60).round()} minutes';
-  } else if (diff.inHours < 24) {
-    return 'Remind me today at ${dateTime.hour.toString().padLeft(2, '0')}'
-        ':${dateTime.minute.toString().padLeft(2, '0')}';
-  } else {
-    return 'Remind me in ${diff.inDays} day${diff.inDays > 1 ? 's' : ''}';
-  }
+  return 'Remind me in ${dateTime.relative()}';
 }
 
 DateTime getDateTimeOfDuration(Duration duration) {
