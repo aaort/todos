bb839df310eed0ff6474cef88cfcc74a5059b4d3
aort <artashesano@icloud.com>
2022-11-08 00:13:41 +0400

    add listener for action on notifications

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -31,19 +31,26 @@ class Notifications {
       // TODO: remove if not testing anymore
       debug: true,
     );
+
+    notifications.setListeners(onActionReceivedMethod: _onActionReceivedMethod);
+  }
+
+  static Future<void> _onActionReceivedMethod(ReceivedAction action) async {
+    // TODO: do required staff
+    print(action.payload);
   }
 
   static scheduleReminder(Todo todo) {
     if (todo.reminderId == null || todo.reminderDateTime == null) return;
     // use date and time with local time zone for more precision
-    final dateTime = tz.TZDateTime.from(todo.reminderDateTime!, tz.local);
     notifications.createNotification(
-      schedule: NotificationCalendar.fromDate(date: dateTime),
+      schedule: NotificationCalendar.fromDate(date: todo.reminderDateTime!),
       content: NotificationContent(
         id: todo.reminderId!,
         channelKey: _notificationChannelKey,
         title: todo.task,
         body: todo.task,
+        payload: {'todoId': todo.id},
         category: NotificationCategory.Reminder,
       ),
       actionButtons: [
