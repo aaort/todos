cf0b1a1c406bf779fa6d432dfe923312be6ca743
aort <artashesano@icloud.com>
2022-11-07 17:46:21 +0400

    use time zone in notifications for reminder precision

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -1,5 +1,7 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:todos/logic/todo.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
 
 const String _notificationChannelKey = 'basic_channel';
 const String _notificationGroupKey = 'basic_channel_group';
@@ -15,6 +17,7 @@ class Notifications {
   static initialize() async {
     final allowed = await notifications.requestPermissionToSendNotifications();
     if (!allowed) return;
+    tz.initializeTimeZones();
     AwesomeNotifications().initialize(
       _appIconDir,
       [
@@ -32,13 +35,16 @@ class Notifications {
 
   static scheduleReminder(Todo todo) {
     if (todo.reminderId == null || todo.reminderDateTime == null) return;
+    // use date and time with local time zone for more precision
+    final dateTime = tz.TZDateTime.from(todo.reminderDateTime!, tz.local);
     notifications.createNotification(
-      schedule: NotificationCalendar.fromDate(date: todo.reminderDateTime!),
+      schedule: NotificationCalendar.fromDate(date: dateTime),
       content: NotificationContent(
         id: todo.reminderId!,
         channelKey: _notificationChannelKey,
         title: todo.task,
         body: todo.task,
+        category: NotificationCategory.Reminder,
       ),
       actionButtons: [
         NotificationActionButton(key: 'COMPLETED', label: 'Mark as completed'),
