9433755fcad1d0d78ff8a7e4f0f666af129ef1fa
aort <artashesano@icloud.com>
2022-11-10 13:05:01 +0400

    store theme mode using shared_preferences to be able to "remember" selected mode

--- lib/theme/theme_manager.dart
+++ lib/theme/theme_manager.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeManager extends ChangeNotifier {
+  ThemeManager() {
+    _initThemeMode();
+  }
+
   ThemeMode _themeMode = ThemeMode.light;
 
   ThemeMode get themeMode => _themeMode;
 
   bool get isDark => _themeMode == ThemeMode.dark;
 
-  toggleTheme() {
+  Future<void> _initThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (prefs.getBool('isDark') == true) {
+      _themeMode = ThemeMode.dark;
+    }
+    notifyListeners();
+  }
+
+  toggleTheme() async {
     _themeMode =
         _themeMode == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isDark', isDark);
     notifyListeners();
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import awesome_notifications
 import path_provider_macos
+import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
