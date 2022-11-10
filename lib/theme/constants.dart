a156bde6a5566d3a2ac3f55f3e75587985603c19
aort <artashesano@icloud.com>
2022-11-10 11:29:25 +0400

    create constants file in theme

---
+++ lib/theme/constants.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Colors.blueGrey;
+const kModalBorderRadius = 20.0;
+const primaryDarkColor = Colors.white;
+final backgroundDarkColor = Colors.black.withOpacity(0.9);
+
+const titleSmall = TextStyle(
+  color: primaryColor,
+  fontSize: 25.0,
+  fontWeight: FontWeight.w600,
+);
+
+const bodyMedium = TextStyle(
+  color: Colors.white,
+  fontSize: 20,
+  fontWeight: FontWeight.w400,
+);
+
+const bodySmall = TextStyle(
+  color: primaryColor,
+  fontSize: 20.0,
+  fontWeight: FontWeight.w400,
+);
+
+const bodyLarge = TextStyle(
+  color: primaryColor,
+  fontSize: 30.0,
+  fontWeight: FontWeight.w600,
+);
+
+final textButtonThemeData = TextButtonThemeData(
+  style: ButtonStyle(
+    textStyle: MaterialStateProperty.resolveWith(getButtonTextStyle),
+  ),
+);
+
+const appBarTheme = AppBarTheme(
+  elevation: 0,
+  titleSpacing: 15.0,
+  backgroundColor: primaryColor,
+  titleTextStyle: TextStyle(
+    color: primaryDarkColor,
+    fontSize: 30.0,
+    fontWeight: FontWeight.w800,
+  ),
+);
+
+const floatingActionButtonThemeData = FloatingActionButtonThemeData(
+  backgroundColor: primaryColor,
+  elevation: 2.0,
+);
+
+const iconThemeData = IconThemeData(
+  color: primaryColor,
+  size: 27,
+);
+
+final bottomSheetThemeData = BottomSheetThemeData(
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.vertical(
+      top: Radius.circular(kModalBorderRadius),
+    ),
+  ),
+  backgroundColor: backgroundDarkColor,
+);
+
+const inputDecorationThemeData = InputDecorationTheme(
+  border: InputBorder.none,
+);
+
+final elevatedButtonThemeData = ElevatedButtonThemeData(
+  style: ButtonStyle(
+    elevation: MaterialStateProperty.resolveWith(getButtonElevation),
+    backgroundColor: MaterialStateProperty.resolveWith(getButtonColor),
+    shape: MaterialStateProperty.all(
+      RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+    ),
+  ),
+);
+
+double getButtonElevation(Set<MaterialState> states) =>
+    states.contains(MaterialState.disabled) ? 2.0 : 0;
+
+Color getButtonColor(Set<MaterialState> states) {
+  if (states.contains(MaterialState.disabled)) {
+    return primaryColor.shade200;
+  }
+  return primaryColor;
+}
+
+TextStyle getButtonTextStyle(Set<MaterialState> states) {
+  if (states.contains(MaterialState.disabled)) {
+    return TextStyle(color: primaryColor.shade200);
+  }
+  return const TextStyle(color: primaryColor);
+}
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-const primaryColor = Colors.blueGrey;
-const kModalBorderRadius = 20.0;
+import 'package:todos/theme/constants.dart';
 
 final lightTheme = ThemeData(
   scaffoldBackgroundColor: primaryColor,
@@ -26,9 +24,6 @@ final lightTheme = ThemeData(
   bottomSheetTheme: bottomSheetThemeData,
 );
 
-const primaryDarkColor = Colors.white;
-final backgroundDarkColor = Colors.black.withOpacity(0.9);
-
 final darkTheme = ThemeData(
   scaffoldBackgroundColor: backgroundDarkColor,
   primaryColor: primaryDarkColor,
@@ -50,94 +45,3 @@ final darkTheme = ThemeData(
   appBarTheme: appBarTheme.copyWith(color: backgroundDarkColor),
   bottomSheetTheme: bottomSheetThemeData,
 );
-
-const titleSmall = TextStyle(
-  color: primaryColor,
-  fontSize: 25.0,
-  fontWeight: FontWeight.w600,
-);
-
-const bodyMedium = TextStyle(
-  color: Colors.white,
-  fontSize: 20,
-  fontWeight: FontWeight.w400,
-);
-
-const bodySmall = TextStyle(
-  color: primaryColor,
-  fontSize: 20.0,
-  fontWeight: FontWeight.w400,
-);
-
-const bodyLarge = TextStyle(
-  color: primaryColor,
-  fontSize: 30.0,
-  fontWeight: FontWeight.w600,
-);
-
-final textButtonThemeData = TextButtonThemeData(
-  style: ButtonStyle(
-    textStyle: MaterialStateProperty.resolveWith(getButtonTextStyle),
-  ),
-);
-
-const appBarTheme = AppBarTheme(
-  elevation: 0,
-  titleSpacing: 15.0,
-  backgroundColor: primaryColor,
-  titleTextStyle: TextStyle(
-    color: primaryDarkColor,
-    fontSize: 30.0,
-    fontWeight: FontWeight.w800,
-  ),
-);
-
-const floatingActionButtonThemeData = FloatingActionButtonThemeData(
-  backgroundColor: primaryColor,
-  elevation: 2.0,
-);
-
-const iconThemeData = IconThemeData(
-  color: primaryColor,
-  size: 27,
-);
-
-final bottomSheetThemeData = BottomSheetThemeData(
-  shape: const RoundedRectangleBorder(
-    borderRadius: BorderRadius.vertical(
-      top: Radius.circular(kModalBorderRadius),
-    ),
-  ),
-  backgroundColor: backgroundDarkColor,
-);
-
-const inputDecorationThemeData = InputDecorationTheme(
-  border: InputBorder.none,
-);
-
-final elevatedButtonThemeData = ElevatedButtonThemeData(
-  style: ButtonStyle(
-    elevation: MaterialStateProperty.resolveWith(getButtonElevation),
-    backgroundColor: MaterialStateProperty.resolveWith(getButtonColor),
-    shape: MaterialStateProperty.all(
-      RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-    ),
-  ),
-);
-
-double getButtonElevation(Set<MaterialState> states) =>
-    states.contains(MaterialState.disabled) ? 2.0 : 0;
-
-Color getButtonColor(Set<MaterialState> states) {
-  if (states.contains(MaterialState.disabled)) {
-    return primaryColor.shade200;
-  }
-  return primaryColor;
-}
-
-TextStyle getButtonTextStyle(Set<MaterialState> states) {
-  if (states.contains(MaterialState.disabled)) {
-    return TextStyle(color: primaryColor.shade200);
-  }
-  return const TextStyle(color: primaryColor);
-}
