b17bb332c277a4c762b430b41e50d217decdf9a6
aort <artashesano@icloud.com>
2022-11-13 13:32:06 +0400

    add material_color file in theme

---
+++ lib/theme/material_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const Map<int, Color> primaryColorMap = {
+  50: Color(0xff3B3936),
+  100: Color(0xff3B3936),
+  200: Color(0xff3B3936),
+  300: Color(0xff3B3936),
+  400: Color(0xff3B3936),
+  500: Color(0xff3B3936),
+  600: Color(0xff3B3936),
+  700: Color(0xff3B3936),
+  800: Color(0xff3B3936),
+  900: Color(0xff3B3936),
+};
+
+const Map<int, Color> primaryDarkColorMap = {
+  50: Color(0xffffffff),
+  100: Color(0xffffffff),
+  200: Color(0xffffffff),
+  300: Color(0xffffffff),
+  400: Color(0xffffffff),
+  500: Color(0xffffffff),
+  600: Color(0xffffffff),
+  700: Color(0xffffffff),
+  800: Color(0xffffffff),
+  900: Color(0xffffffff),
+};
