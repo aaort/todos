6059edbd6b39f5ebe89b86ae4856d245fd6fc3dc
aort <artashesano@icloud.com>
2022-11-14 13:27:00 +0400

    implement isKeyboardVisible helper method

--- lib/helpers/keyboard.dart
+++ lib/helpers/keyboard.dart
@@ -4,3 +4,7 @@ Future<void> hideKeyboardAndWait() async {
   FocusManager.instance.primaryFocus?.unfocus();
   await Future.delayed(const Duration(milliseconds: 400));
 }
+
+bool isKeyboardVisible(BuildContext ctx) {
+  return MediaQuery.of(ctx).viewInsets.bottom > 0;
+}
