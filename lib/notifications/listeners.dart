c522e811e8caf1ae5db2028bcf42afb154400d48
aort <artashesano@icloud.com>
2022-11-14 22:39:06 +0400

    implement repeat in 5 and 15 minutes notification actions

--- lib/notifications/listeners.dart
+++ lib/notifications/listeners.dart
@@ -1,5 +1,6 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:provider/provider.dart';
+import 'package:todos/logic/todo_actions.dart';
 import 'package:todos/logic/todos.dart';
 import 'package:todos/logic/todos_io.dart';
 import 'package:todos/main.dart';
@@ -8,12 +9,23 @@ import 'package:todos/notifications/constants.dart';
 Future<void> onActionReceivedMethod(ReceivedAction action) async {
   final todoId = action.payload?['todoId'];
   if (todoId == null) return;
+  final context = App.materialAppKey.currentContext;
   if (action.buttonKeyPressed == notificationActions[completedButtonKey]) {
     if (action.actionLifeCycle != NotificationLifeCycle.AppKilled) {
-      App.materialAppKey.currentContext
-          ?.read<Todos>()
-          .toggleCheckById(todoId, value: true);
+      context?.read<Todos>().toggleCheckById(todoId, value: true);
     }
     TodosIO.toggleCheck(action.payload!['todoId']!, value: true);
+  } else {
+    late Duration duration;
+    if (action.buttonKeyPressed == notificationActions[in5MinutesButtonKey]) {
+      duration = const Duration(minutes: 5);
+    } else {
+      duration = const Duration(minutes: 15);
+    }
+    final newReminder = DateTime.now().add(duration);
+    final todo = context?.read<Todos>().getTodoById(todoId);
+    if (context != null && todo != null) {
+      TodoActions(context, todo).updateReminder(newReminder);
+    }
   }
 }
