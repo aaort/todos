2735cd82938aa251c4885153e33fafde753ed859
aort <artashesano@icloud.com>
2022-11-17 13:05:18 +0400

    implement logout button

---
+++ lib/widgets/logout_button.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:todos/auth/app_navigator.dart';
+
+class LogoutButton extends StatelessWidget {
+  const LogoutButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    onLogout() async {
+      FirebaseAuth.instance.signOut().then((_) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => const AppNavigator()),
+          (route) => false,
+        );
+      });
+    }
+
+    return IconButton(
+        onPressed: onLogout, icon: const Icon(Icons.logout_outlined));
+  }
+}
