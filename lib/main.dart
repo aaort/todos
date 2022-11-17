ac464716636774d27ebb7648909ee0773d6efdce
aort <artashesano@icloud.com>
2022-11-17 19:17:04 +0400

    remove unused imports in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todos/navigation/app_navigator.dart';
 import 'package:todos/logic/todos.dart';
+import 'package:todos/navigation/app_navigator.dart';
 import 'package:todos/notifications/notifications.dart';
 import 'package:todos/theme/theme.dart';
 import 'package:todos/theme/theme_manager.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
 
 void main() async {
