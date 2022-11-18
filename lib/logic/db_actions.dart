0bc8ad48825a45e3934261b794885477cae1aa49
aort <artashesano@icloud.com>
2022-11-18 19:01:32 +0400

    add static _todos as todos collection for current user

--- lib/logic/db_actions.dart
+++ lib/logic/db_actions.dart
@@ -6,41 +6,26 @@ class DbActions {
   static final _db = FirebaseFirestore.instance;
   static final _auth = FirebaseAuth.instance;
 
+  static final _todos =
+      _db.collection('users').doc(_auth.currentUser?.uid).collection('todos');
+
   static createTodo(Todo todo) {
-    _db
-        .collection('users')
-        .doc(_auth.currentUser?.uid)
-        .collection('todos')
-        .doc(todo.id)
-        .set(todo.asMap);
+    _todos.doc(todo.id).set(todo.asMap);
   }
 
   static updateTodo(Todo updatedTodo) {
-    _db
-        .collection('users')
-        .doc(_auth.currentUser?.uid)
-        .collection('todos')
-        .doc(updatedTodo.id)
-        .set(updatedTodo.asMap);
+    _todos.doc(updatedTodo.id).set(updatedTodo.asMap);
   }
 
   static Stream<QuerySnapshot<Map>> getTodos() {
-    return _db
-        .collection('users')
-        .doc(_auth.currentUser?.uid)
-        .collection('todos')
-        .snapshots();
+    return _todos.snapshots();
   }
 
   static Future<int> getTodosCount() async {
-    return (await _db
-            .collection('users')
-            .doc(_auth.currentUser?.uid)
-            .collection('todos')
-            .get())
-        .docs
-        .length;
+    return (await _todos.get()).docs.length;
   }
 
-  deleteTodo(String id) {}
+  static deleteTodo(String id) async {
+    await _todos.doc(id).delete();
+  }
 }
