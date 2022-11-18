1c769c0dcd68118e33ca4a7b8cbe8361b95660ed
aort <artashesano@icloud.com>
2022-11-18 22:23:23 +0400

    add user to database after user created in userActions

--- lib/logic/user_actions.dart
+++ lib/logic/user_actions.dart
@@ -1,18 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class UserActions {
   static final _auth = FirebaseAuth.instance;
+  static final _db = FirebaseFirestore.instance;
 
-  static logout() async => await _auth.signOut();
+  static Future<void> logout() async {
+    await _auth.signOut();
+  }
 
   static login({required String email, required String password}) async {
     await _auth.signInWithEmailAndPassword(email: email, password: password);
   }
 
   static createUser({required String email, required String password}) async {
-    await _auth.createUserWithEmailAndPassword(
+    final credentials = await _auth.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
+    if (credentials.user == null) return;
+
+    final user = {
+      'email': credentials.user!.email,
+      'id': credentials.user!.uid,
+    };
+
+    await _db.collection('users').doc(user['id']).set(user);
   }
 }
