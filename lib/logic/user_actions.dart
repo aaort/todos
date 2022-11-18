6d71112c8f334b02bbd53114f5cef63cb6465bc3
aort <artashesano@icloud.com>
2022-11-18 21:00:15 +0400

    add login and createUser methods in userActions

--- lib/logic/user_actions.dart
+++ lib/logic/user_actions.dart
@@ -4,4 +4,15 @@ class UserActions {
   static final _auth = FirebaseAuth.instance;
 
   static logout() async => await _auth.signOut();
+
+  static login({required String email, required String password}) async {
+    await _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  static createUser({required String email, required String password}) async {
+    await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
 }
