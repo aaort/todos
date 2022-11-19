6c37dad94b0e7cb3bf4222e80322a1c0e20d7b56
aort <artashesano@icloud.com>
2022-11-19 12:01:50 +0400

    use list instead of set in auth.constants

--- lib/auth/constants.dart
+++ lib/auth/constants.dart
@@ -1,14 +1,13 @@
-final Set authExceptions = <String>{
+final authExceptions = <String>[
   'wrong-password',
   'weak-password',
   'email-already-in-use',
   'user-not-found',
-};
+];
 
 final Map errorMessages = <String, String>{
-  authExceptions.elementAt(0): 'Provided password is incorrect',
-  authExceptions.elementAt(1): 'Provided password is too weak',
-  authExceptions.elementAt(2): 'Provided email is already in use',
-  authExceptions.elementAt(3):
-      "There is no user found with provided credentials",
+  authExceptions[0]: 'Provided password is incorrect',
+  authExceptions[1]: 'Provided password is too weak',
+  authExceptions[2]: 'Provided email is already in use',
+  authExceptions[3]: "There is no user found with provided credentials",
 };
