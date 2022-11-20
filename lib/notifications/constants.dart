cf5f1dffc5b0ecfe95459b793dba5a4402830021
aort <artashesano@icloud.com>
2022-11-20 16:44:16 +0400

    add "cancel" button in notifications/constants

--- lib/notifications/constants.dart
+++ lib/notifications/constants.dart
@@ -12,11 +12,13 @@ const String appIconPath = 'resource://drawable/ic_launcher';
 const String completedButtonKey = 'COMPLETED';
 const String in5MinutesButtonKey = 'IN_5_MINUTES';
 const String in15MinutesButtonKey = 'IN_15_MINUTES';
+const String cancelButtonKey = "CANCEL";
 
 const Map<String, String> notificationActions = {
   completedButtonKey: completedButtonKey,
   in5MinutesButtonKey: in5MinutesButtonKey,
   in15MinutesButtonKey: in15MinutesButtonKey,
+  cancelButtonKey: cancelButtonKey,
 };
 
 final actionButtons = [
@@ -33,6 +35,11 @@ final actionButtons = [
   NotificationActionButton(
     key: notificationActions[in15MinutesButtonKey]!,
     label: 'Repeat in 15 minutes',
+    actionType: ActionType.SilentAction,
+  ),
+  NotificationActionButton(
+    key: notificationActions[cancelButtonKey]!,
+    label: 'Cancel',
     actionType: ActionType.SilentAction,
   ),
 ];
