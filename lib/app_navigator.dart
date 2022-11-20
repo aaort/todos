8f93110c368eecca7eb74f82a65d03834aff1066
aort <artashesano@icloud.com>
2022-11-20 15:11:27 +0400

    use LoadingIndicator widget in app_navigator and todo_list

--- lib/app_navigator.dart
+++ lib/app_navigator.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:todos/auth/sign_in.dart';
 import 'package:todos/screens/home.dart';
+import 'package:todos/widgets/loading_indicator.dart';
 
 class AppNavigator extends StatelessWidget {
   const AppNavigator({super.key});
@@ -12,11 +13,7 @@ class AppNavigator extends StatelessWidget {
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: ((context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(
-              color: Theme.of(context).primaryColor,
-            ),
-          );
+          return const LoadingIndicator();
         } else if (snapshot.data is User) {
           return const Home();
         }
