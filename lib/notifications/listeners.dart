95350e2d51de7a493a5467175647f30a79edd057
aort <artashesano@icloud.com>
2022-11-21 23:21:19 +0400

    code improvements and fixes on listeners

--- lib/notifications/listeners.dart
+++ lib/notifications/listeners.dart
@@ -1,5 +1,5 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
-import 'package:todos/extensions.dart' show MinutePrecision;
+import 'package:todos/extensions.dart';
 import 'package:todos/logic/todo_functions.dart';
 import 'package:todos/notifications/constants.dart';
 import 'package:todos/notifications/notifications.dart';
@@ -8,21 +8,19 @@ import 'package:todos/notifications/notifications.dart';
 Future<void> onActionReceived(ReceivedAction action) async {
   if (action.payload?['todoId'] == null) return;
   final todo = await TodoFunctions.getTodoById(action.payload!['todoId']!);
-  if (todo == null || todo.reminderId == null) return;
+  if (todo == null) return;
   if (action.buttonKeyPressed == notificationActions[completedButtonKey]) {
-    TodoFunctions(todo).toggleIsDone();
-    if (todo.repeat == null) {
+    todo.toggleIsDone();
+    if (todo.repeat == null && todo.reminderId != null) {
       // if not a repeating reminder, cancel it
       Notifications.cancelReminder(todo.reminderId!);
-      todo.updateReminder(null); // drop reminder
-      TodoFunctions(todo).updateTodo();
+      TodoFunctions(todo..updateReminder(null)).updateTodo();
     }
   } else if (action.buttonKeyPressed != notificationActions[cancelButtonKey]) {
     final is5Minutes =
         action.buttonKeyPressed == notificationActions[in5MinutesButtonKey];
-    final reminder = DateTime.now()
-        .add(Duration(minutes: is5Minutes ? 5 : 15))
-        .toMinutePrecision();
+    final reminder =
+        Duration(minutes: is5Minutes ? 5 : 15).toDateTime().toMinutePrecision();
     final updatedTodo = todo.copyWith({'reminder': reminder});
     TodoFunctions(updatedTodo).updateTodo();
     Notifications.updateReminder(updatedTodo);
