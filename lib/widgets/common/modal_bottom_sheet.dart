9219191b866505eb1f3232258675199b015be3fb
aort <artashesano@icloud.com>
2022-11-22 13:51:58 +0400

    add shouldConfirmPop property on popupModalBottomSheet

--- lib/widgets/common/modal_bottom_sheet.dart
+++ lib/widgets/common/modal_bottom_sheet.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:todos/widgets/todo_editor/confirm_discard.dart';
 
-void popupModalBottomSheet({
+Future<void> popupModalBottomSheet({
   required BuildContext context,
   required Widget child,
+  bool shouldConfirmPop = false,
   EdgeInsets? padding,
-}) {
-  showModalBottomSheet<void>(
+}) async {
+  await showModalBottomSheet<void>(
     context: context,
     isScrollControlled: true,
-    builder: (context) => Padding(
-      padding: padding ?? const EdgeInsets.symmetric(vertical: 10.0),
-      child: child,
+    builder: (context) => WillPopScope(
+      onWillPop: () async {
+        if (!shouldConfirmPop) return Future.value(true);
+        final value = await showConfirmDiscard(context);
+        return value;
+      },
+      child: Padding(
+        padding: padding ?? const EdgeInsets.symmetric(vertical: 10.0),
+        child: child,
+      ),
     ),
   );
 }
