203c4be32a6b40b9ad8013492eb6143e9f25dd98
aort <artashesano@icloud.com>
2022-11-22 18:05:46 +0400

    make onActionReceived a static method

--- lib/notifications/listeners.dart
+++ lib/notifications/listeners.dart
@@ -4,27 +4,30 @@ import 'package:todos/logic/todo_functions.dart';
 import 'package:todos/notifications/constants.dart';
 import 'package:todos/notifications/notifications.dart';
 
-@pragma("vm:entry-point")
-Future<void> onActionReceived(ReceivedAction action) async {
-  if (action.payload?['todoId'] == null) return;
-  final todo = await TodoFunctions.getTodoById(action.payload!['todoId']!);
-  if (todo == null || todo.reminderId == null) return;
-  if (action.buttonKeyPressed == notificationActions[completedButtonKey]) {
-    todo.toggleIsDone();
-    if (todo.repeat == null) {
-      // if not a repeating reminder, cancel it
+class NotificationListeners {
+  @pragma("vm:entry-point")
+  static Future<void> onActionReceived(ReceivedAction action) async {
+    if (action.payload?['todoId'] == null) return;
+    final todo = await TodoFunctions.getTodoById(action.payload!['todoId']!);
+    if (todo == null || todo.reminderId == null) return;
+    if (action.buttonKeyPressed == notificationActions[completedButtonKey]) {
+      todo.toggleIsDone();
+      if (todo.repeat == null) {
+        // if not a repeating reminder, cancel it
+        Notifications.cancelReminder(todo.reminderId!);
+        TodoFunctions(todo..updateReminder(null)).updateTodo();
+      }
+    } else if (action.buttonKeyPressed !=
+        notificationActions[cancelButtonKey]) {
+      final is5Minutes =
+          action.buttonKeyPressed == notificationActions[in5MinutesButtonKey];
+      todo.updateReminder(Duration(minutes: is5Minutes ? 5 : 15)
+          .toDateTime()
+          .toMinutePrecision());
+      TodoFunctions(todo).updateTodo();
+      Notifications.updateReminder(todo);
+    } else {
       Notifications.cancelReminder(todo.reminderId!);
-      TodoFunctions(todo..updateReminder(null)).updateTodo();
     }
-  } else if (action.buttonKeyPressed != notificationActions[cancelButtonKey]) {
-    final is5Minutes =
-        action.buttonKeyPressed == notificationActions[in5MinutesButtonKey];
-    todo.updateReminder(Duration(minutes: is5Minutes ? 5 : 15)
-        .toDateTime()
-        .toMinutePrecision());
-    TodoFunctions(todo).updateTodo();
-    Notifications.updateReminder(todo);
-  } else {
-    Notifications.cancelReminder(todo.reminderId!);
   }
 }
