4631af0ce13c9c42d00f528d40b8dec0092dc42e
aort <artashesano@icloud.com>
2022-11-22 13:52:08 +0400

    implement confirm_discard widget

---
+++ lib/widgets/todo_editor/confirm_discard.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:todos/widgets/common/pickers.dart';
+
+final options = [
+  PickerOption<bool>('Discard', true),
+  PickerOption<bool>('Cancel', false),
+];
+
+Future<bool> showConfirmDiscard(BuildContext context) async {
+  bool discard = false;
+
+  void onChange(bool newValue) {
+    discard = newValue;
+    Navigator.pop(context);
+  }
+
+  await showOptionPicker<bool>(
+    context: context,
+    title: 'Are you sure ?',
+    options: options,
+    onChange: onChange,
+  );
+
+  if (discard) await Future.delayed(const Duration(milliseconds: 300));
+  return discard;
+}
