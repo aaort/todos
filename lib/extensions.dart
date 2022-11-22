a11240c83d6dfa160f9c1888a189187d5fbb0757
aort <artashesano@icloud.com>
2022-11-22 14:31:45 +0400

    remove CustomTextStyles extension on TextTheme

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,13 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:todos/widgets/todo_editor/repeat_button.dart';
 
 extension Stringify on Repeat {
   String toName() => toString().split('.').last;
-}
-
-extension CustomTextStyles on TextTheme {
-  TextStyle get lineThrough =>
-      bodySmall!.copyWith(decoration: TextDecoration.lineThrough);
 }
 
 extension Reminder on Duration {
