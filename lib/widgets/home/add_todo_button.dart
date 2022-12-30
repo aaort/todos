c31115a768fa0204758c667e911b6e51b806bcd2
aort <artashesano@icloud.com>
2022-12-30 16:04:36 +0400

    Remove custom animation of AddTodoButton

--- lib/widgets/home/add_todo_button.dart
+++ lib/widgets/home/add_todo_button.dart
@@ -2,23 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:todos/screens/todo_editor.dart';
 import 'package:todos/widgets/common/modal_bottom_sheet.dart';
 
-class AddTodoButton extends StatefulWidget {
+class AddTodoButton extends StatelessWidget {
   const AddTodoButton({super.key});
 
-  @override
-  State<AddTodoButton> createState() => _AddTodoButtonState();
-}
-
-class _AddTodoButtonState extends State<AddTodoButton>
-    with TickerProviderStateMixin {
-  late final _animController = AnimationController(
-    duration: const Duration(milliseconds: 500),
-    vsync: this,
-  );
-  late final _animation =
-      CurvedAnimation(parent: _animController, curve: Curves.linear);
-
-  showAddTodoModal() {
+  showAddTodoModal(BuildContext context) {
     popupModalBottomSheet(
       context: context,
       child: const TodoEditor(),
@@ -26,25 +13,10 @@ class _AddTodoButtonState extends State<AddTodoButton>
   }
 
   @override
-  void initState() {
-    _animController.forward();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return ScaleTransition(
-      scale: _animation,
-      child: FloatingActionButton(
-        onPressed: showAddTodoModal,
-        child: const Icon(Icons.edit_outlined),
-      ),
+    return FloatingActionButton(
+      onPressed: () => showAddTodoModal(context),
+      child: const Icon(Icons.edit_outlined),
     );
-  }
-
-  @override
-  void dispose() {
-    _animController.dispose();
-    super.dispose();
   }
 }
