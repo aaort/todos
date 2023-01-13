e74bda994196bdf5c60c566f1406026bc5c8f85f
aort <artashesano@icloud.com>
2023-01-13 08:11:58 +0400

    Extract logic in notifications/listeners and add comments

--- lib/notifications/listeners.dart
+++ lib/notifications/listeners.dart
@@ -1,5 +1,5 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
-import 'package:todos/extensions.dart';
+import 'package:todos/notifications/utils.dart';
 import 'package:todos/services/database.dart';
 import 'package:todos/notifications/constants.dart';
 import 'package:todos/notifications/notifications.dart';
@@ -8,25 +8,18 @@ class NotificationListeners {
   @pragma("vm:entry-point")
   static Future<void> onActionReceived(ReceivedAction action) async {
     if (action.payload?['todoId'] == null) return;
+
+    // Get todo by id and return functions if null or does not have reminderId
     final todo = await Database.getTodoById(action.payload!['todoId']!);
     if (todo == null || todo.reminderId == null) return;
+
+    // If todo's marked as completed
     if (action.buttonKeyPressed == notificationActions[completedButtonKey]) {
-      final toggledTodo = todo.toggleIsDone();
-      if (toggledTodo.repeat == null) {
-        // if not a repeating reminder, cancel it
-        Notifications.cancelReminder(todo.reminderId!);
-        Database(toggledTodo.updateReminder(null)).updateTodo();
-      }
+      await toggleTodo(todo);
+      // If not marked as [CANCELED] then todo's rescheduled with either 5 or 15 minutes
     } else if (action.buttonKeyPressed !=
         notificationActions[cancelButtonKey]) {
-      final is5Minutes =
-          action.buttonKeyPressed == notificationActions[in5MinutesButtonKey];
-      final updatedReminderTodo = todo.updateReminder(
-          Duration(minutes: is5Minutes ? 5 : 15)
-              .toDateTime()
-              .toMinutePrecision());
-      Database(updatedReminderTodo).updateTodo();
-      Notifications.updateReminder(updatedReminderTodo);
+      await updateReminder(todo: todo, action: action);
     } else {
       Notifications.cancelReminder(todo.reminderId!);
     }
---
+++ lib/notifications/utils.dart
@@ -0,0 +1,28 @@
+import 'package:awesome_notifications/awesome_notifications.dart'
+    show ReceivedAction;
+import 'package:todos/extensions.dart' show Reminder, MinutePrecision;
+import 'package:todos/models/todo.dart' show Todo;
+import 'package:todos/notifications/notifications.dart';
+import 'package:todos/notifications/constants.dart'
+    show in5MinutesButtonKey, notificationActions;
+import 'package:todos/services/database.dart';
+
+// Those methods supposed to be used only inside ./listeners.dart file so far
+
+toggleTodo(Todo todo) async {
+  final toggledTodo = todo.toggleIsDone();
+  if (toggledTodo.repeat == null) {
+    // if not a repeating reminder, cancel it
+    Notifications.cancelReminder(todo.reminderId!);
+    Database(toggledTodo.updateReminder(null)).updateTodo();
+  }
+}
+
+updateReminder({required Todo todo, required ReceivedAction action}) async {
+  final is5Minutes =
+      action.buttonKeyPressed == notificationActions[in5MinutesButtonKey];
+  final updatedReminderTodo = todo.updateReminder(
+      Duration(minutes: is5Minutes ? 5 : 15).toDateTime().toMinutePrecision());
+  Database(updatedReminderTodo).updateTodo();
+  Notifications.updateReminder(updatedReminderTodo);
+}
