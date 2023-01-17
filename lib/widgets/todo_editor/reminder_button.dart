0d2df52edc79cad72e3eca9b0547754a49a67289
aort <artashesano@icloud.com>
2023-01-17 17:02:41 +0400

    Temporary remove duration options for reminder

--- lib/widgets/todo_editor/reminder_button.dart
+++ lib/widgets/todo_editor/reminder_button.dart
@@ -12,9 +12,9 @@ enum ReminderOption {
   custom,
 }
 
-const _reminderOptions = <PickerOption<dynamic>>[
-  PickerOption('In 5 minutes', Duration(minutes: 5)),
-  PickerOption('In 15 minutes', Duration(minutes: 15)),
+const _reminderOptions = <PickerOption<ReminderOption>>[
+  // PickerOption('In 5 minutes', Duration(minutes: 5)),
+  // PickerOption('In 15 minutes', Duration(minutes: 15)),
   PickerOption('Custom', ReminderOption.custom),
 ];
 
