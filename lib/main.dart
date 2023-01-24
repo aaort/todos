b20332ae1d0533807b849c15ed8b4239f5a1770a
aort <artashesano@icloud.com>
2023-01-24 19:49:08 +0400

    Rename theme_manager to match inner class name

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todos/app_navigator.dart';
 import 'package:todos/theme/theme.dart';
-import 'package:todos/theme/theme_manager.dart';
+import 'package:todos/theme/theme_mode_manager.dart';
 
 import 'firebase_options.dart';
 
